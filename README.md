d75e891621fa1f2b51e49719c1ed206d445094cc
Michael <mywyaudev@gmail.com>
2024-12-24 20:35:11 +0000

    wander-39 update read me and wip adding code for office specifications (#9)
    * add office contact details page
    
    * add specifications defaut values and schema
    
    * adding specifications inputs
    
    * wip fixing specifications
    
    * adding all fields for specs
    
    * wip fixing page, need to handle start and closing time in a better way
    
    * update readme
    
    ---------
    
    Co-authored-by: mywyau <47248104+mywyau@users.noreply.github.com>

--- README.md
+++ README.md
@@ -5,18 +5,18 @@
 Frontend Repo/User Interface for wanderer user domain specific functions and services.
 
 
-## Running the App
+## Building & running the App in docker container locally
 
 ### Dev
 
 ```
- ./docker_compose_dev.sh
+ ./run_dev.sh
 ```
 
 ### Production
 
 ```
- ./docker_compose_prod.sh
+ ./run_prod.sh
 ```
 
 ## Running tests
@@ -27,7 +27,7 @@ Frontend Repo/User Interface for wanderer user domain specific functions and ser
 
 ### To run a single unit test suite
 
-Note you may need to use absolute paths but jest is pretty good in handling relative paths
+Note: you may need to use absolute paths but jest is pretty good in handling relative paths
 
 ```
  ./run_tests.sh  <path to test suite file>   
@@ -35,9 +35,10 @@ Note you may need to use absolute paths but jest is pretty good in handling rela
 
 ## App configs
 
-Configuration values are handled in the .env files
+Configuration values are handled for now in the .env files
 
-There is a inheritence/heirarchy type logic to these files 
+There is a inheritence/heirarchy type logic to these files but need to google the .env file names again.
+Can make one for local, qa and prod
 
 TODO: document exmaple
 
@@ -55,10 +56,57 @@ use [id] in the path for query parameters
 
 ### Components and types
 
-Components and types for code locality sake can be defined in the final directory as components/types directories this should help mentally keeping track of page    related components and interfaces/types.
+Components and types for helping with various aspects of the app code are defined outside of the app. This is to help keep the app page routes files and directories cleaner. 
 
-please delete any unrelated code and likely unused code, please keep comments to a minimal and program in a functional declarative manner. 
+Components are store e.g. 
+
+```
+./src/components
+```
+
+Interfaces for various aspecs such as json requests and responses can be found in Types:
+```
+./src/types
+```
+
+
+---
+
+Please delete any unrelated code and likely unused code, please keep comments to a minimal and program in ideally a functional declarative manner. 
  
 
 Notes:
 Thank you for reading :)
+
+
+Kurtis 
+
+Basically the idea is to build a desk/office booking app. Kinda like a air bnb but for desks. Know it's already been done etc. but wanting to learn more about architecture, design, testing, making tooling choices and solo dev choices etc. Also can eventually maybe look into other things such as event driven architecture using kafka/message queues and web hooks. At one point did have a streaming web hook feature in place for desk availabiliy and changing state for a given desk but moved it to another github repo/microservice, and will likely scrap it or change it entirely. 
+
+The stack is fairly functional:
+
+- Postgresql
+
+- Scala Backend - for features like CRUD operations to create data in sql
+- Scala Backend - webhook servers/kafka pub sub messaging systems - have a few fairly basic examples. Like a publisher, and test consumer to allow sending messages between systems via kafka. But all it does is send a message
+- Scala Backend is a Typelevel stack aka: Http4s (webserver + endpoints), Doobie (connecting to Postgresql), Circes (json handling, for generic derivation etc.)
+- Testing at the moment is Weaver Tests again Scala Typelevel ecosystem
+- Unit and Integration tests, e.g. CRUD operations creating real sql data in a test db and hitting endpoints by spinning up test server and closing down after.
+- Logging is handled using Log4Cats which I think mainly acts as a wrapper for Slf4jLogger not sure tho.
+- For configuration files use PureConfig. Basically can read in your configuration files as Scala code and add typing so helps reduce bugs in config. Trying to move host and ports, or feature switches and values that can be configured for different environments into configuration files but it's mostly work in progress.
+
+- NextJs for my react frontend. 
+- Tailwind CSS - basically for inline css and a consistent/easy way of handling styling. 
+- Form validation uses a library called Zod 
+
+
+Not sure you are able to run the app but ideally want to containerise all the microservices I have on hand frontend and backend and put it on a shared docker network etc. 
+
+Due to storage of jwts for auth it is based on the base domain, need to look into reverse proxy or somehow proxying all base domains to look like a single base domain for the app. As it stands auth only works in another microservice called Reggie.
+
+Mising a lot of unit tests, integration tests, just can't be bothered atm. And code changes a with significant rewrites etc. 
+
+
+Let me know if there are any questions. 
+
+
--- src/app/business/office/specifications/add/page.tsx
+++ src/app/business/office/specifications/add/page.tsx
@@ -1,72 +1,221 @@
 "use client";
 
+import Amenities from "@/components/office/Amenities";
+import NumberInput from "@/components/office/NumberInput";
+import OpeningHours from "@/components/office/OpeningHours";
+import SelectField from "@/components/office/SelectField";
+import TextArea from "@/components/office/TextArea";
+import TextInput from "@/components/office/TextInput";
+import AppConfig from "@/config/AppConfig";
+import { officeSpecificationsSchema } from "@/forms/office/OfficeSpecificationsFormSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FormProvider, useForm } from "react-hook-form";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
-import OfficeSpecsForm from "./forms/OfficeSpecsForm";
-import AddressDetailsForm from "./forms/AddressDetailsForm";
-import ContactDetailsForm from "./forms/ContactDetailsForm";
-import { combinedSchema } from "./helpers/combinedSchema";
 
-type CombinedFormData = z.infer<typeof combinedSchema>;
 
-const AddOfficePage = () => {
+//  note to self sort out database time issues, scala backend data model using LocalTime and look into 
+//  npm install dayjs for frontend time handling
+
+const AddOfficeSpecificationsPage = () => {
+
+  type OfficeSpecifications = z.infer<typeof officeSpecificationsSchema>;
 
   const defaultValues = {
-    officeSpecs: {
-      officeName: "",
-      description: "",
-      officeType: "",
-      numberOfFloors: "",
-      capacity: "",
-      totalDesks: "",
-      amenities: [],
-      availability: {
-        days: [],
-        startTime: "09:00",
-        endTime: "17:00",
-      },
-      rules: "",
+    officeName: "",
+    description: "",
+    officeType: "",
+    numberOfFloors: 0,
+    totalDesks: 0,
+    capacity: 0,
+    amenities: [],
+    availability: {
+      days: [],
+      startTime: "09:00",
+      endTime: "17:00",
+    },
+    rules: "",
+  };
+
+  // React Hook Form Methods
+  const methods = useForm<OfficeSpecifications>({
+    resolver: zodResolver(officeSpecificationsSchema),
+    defaultValues,
+    mode: "onSubmit",
+  });
+
+  const [submitError, setSubmitError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  const onSubmit = async (data: OfficeSpecifications) => {
+
+    const pistachioUrl = AppConfig.basePistachioUrl(false);
+
+    console.log("onSubmit called");
+    console.log("Form Data:", data);
+    console.log(`http://${pistachioUrl}/pistachio/business/offices/specifications/create`)
+
+    setSubmitError(null); // Reset error before submitting
+    setSuccessMessage(null); // Reset success message before submitting
+
+    const combinedData = {
+      ...data,
+      id: 100,
+      businessId: "BUS123456",
+      officeId: "OFF123456",
+      createdAt: new Date().toISOString().slice(0, 19),
+      updatedAt: new Date().toISOString().slice(0, 19)
+    };
+
+
+    console.log(combinedData);
+
+    try {
+      const response = await fetch(
+        `http://${pistachioUrl}/pistachio/business/offices/specifications/create`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(combinedData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to submit form");
+      }
+
+      const responseData = await response.json();
+      console.log("Successfully submitted:", responseData);
+      setSuccessMessage("Form submitted successfully!");
+      methods.reset(); // Reset form fields after successful submission
+    } catch (error) {
+      console.error("Submission error:", error);
+      setSubmitError("Failed to submit the form. Please try again.");
     }
   };
 
-  const methods = useForm({
-    resolver: zodResolver(combinedSchema),
-    defaultValues,
-    // mode: "onChange", // Validation triggers on each change
-  });
+  const {
+    register,
+    formState: { errors },
+  } = methods;
 
-  const onSubmit = (data: CombinedFormData) => {
-    console.log("Combined Data:", data);
-
-    fetch("/api/offices", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to submit form");
-        }
-        return response.json();
-      })
-      .then((data) => console.log("Successfully submitted:", data))
-      .catch((error) => console.error("Submission error:", error));
-  };
+  const amenitiesList = ["Air Conditioning", "Coffee", "Food", "Printer", "Parking", "Quiet Zones", "WiFi"];
 
   return (
-    <FormProvider {...methods}>
+    <div>
       <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-6">
-        <h1 className="text-xl font-bold">Add Office</h1>
+        <h1 className="text-xl font-bold">Add specifications to the office</h1>
 
-        <OfficeSpecsForm />
+        {submitError && <p className="text-red-500">{submitError}</p>}
+        {successMessage && <p className="text-green-500">{successMessage}</p>}
 
-        <button type="submit" className="btn-primary">
-          Submit All
+        <div className="space-y-4">
+          <div className="grid grid-cols-1 gap-6">
+
+            <TextInput
+              id="officeName"
+              name="officeName"
+              label="Office Name"
+              placeholder="Enter a name for the office"
+              register={register}
+              error={errors?.officeName?.message}
+              inputClassName="w-1/2"
+            />
+
+            <TextArea
+              id="description"
+              name="description"
+              label="Office Description"
+              register={register}
+              placeholder="Short description of the office"
+              error={errors.description?.message}
+            />
+
+            <TextArea
+              id="rules"
+              name="rules"
+              label="Office Rules"
+              register={register}
+              placeholder="Enter any rules you want for the office"
+              error={errors.rules?.message}
+            />
+
+            <SelectField
+              id="officeType"
+              name="officeType"
+              label="Type of Office"
+              register={register}
+              options={[
+                { value: "CoworkingSpace", label: "Coworking Space" },
+                { value: "OpenPlanOffice", label: "Open Plan Office" },
+                { value: "PrivateOffice", label: "Private Office" },
+                { value: "ExecutiveOffice", label: "Executive Office" },
+                { value: "MeetingRoom", label: "Meeting Room" },
+              ]}
+              error={errors.officeType}
+            />
+
+            <div className="w-1/2">
+              <div className="grid grid-cols-3 gap-4">
+
+                <NumberInput
+                  id="numberOfFloors"
+                  name="numberOfFloors"
+                  label="Number Of Floors"
+                  register={register}
+                  placeholder="Enter the number of floors"
+                  error={errors.numberOfFloors?.message}
+                  inputClassName="w-full"
+                />
+
+                <NumberInput
+                  id="capacity"
+                  name="capacity"
+                  label="Capacity"
+                  register={register}
+                  placeholder="Enter the capacity of the office"
+                  error={errors.capacity?.message}
+                  inputClassName="w-full"
+                />
+
+                <NumberInput
+                  id="totalDesks"
+                  name="totalDesks"
+                  label="Total Number of Desks"
+                  register={register}
+                  placeholder="Enter the total number of desks in the office"
+                  error={errors.totalDesks?.message}
+                  inputClassName="w-full"
+                />
+              </div>
+            </div>
+
+            <Amenities
+              amenities={amenitiesList}
+              name="amenities"
+              register={register}
+              error={errors.amenities?.message}
+            />
+
+            <OpeningHours
+              days={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]}
+              namePrefix="availability"
+              register={register}
+              errors={errors.availability}
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="btn-primary w-1/3 bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 transition"
+        >
+          Submit
         </button>
+
       </form>
-    </FormProvider>
+    </div>
   );
 };
 
-export default AddOfficePage;
+export default AddOfficeSpecificationsPage;
