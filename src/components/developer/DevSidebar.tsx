1a9f5a8eecc7e91a0765bd69bedd850a3c9f0312
mywyau <47248104+mywyau@users.noreply.github.com>
2024-12-26 23:27:06 +0000

    clean up developer page

--- src/components/developer/DevSidebar.tsx
+++ src/components/developer/DevSidebar.tsx
@@ -49,16 +49,6 @@ export default function Sidebar({ isOpen }: { isOpen: boolean }) {
                 <ul className="space-y-2 font-medium">
                     <li>
                         <Link
-                            href="/wanderer/account/profile"
-                            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group hover:text-indigo-700"
-                        >
-                            {/* <svg className="w-5 h-5 text-gray-500 group-hover:text-gray-900" fill="currentColor" viewBox="0 0 22 21">
-                                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                            </svg> */}
-                            <span className={`${isCollapsed ? "hidden" : ""} ms-3`}>Profile</span>
-                        </Link>
-                        <Link
                             href="/"
                             className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group hover:text-indigo-700"
                         >
@@ -77,28 +67,16 @@ export default function Sidebar({ isOpen }: { isOpen: boolean }) {
                             <span className={`${isCollapsed ? "hidden" : ""} ms-3`}>Business Profile</span>
                         </Link>
                         <Link
-                            href="/time-selection"
+                            href="/user/account/profile"
                             className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group hover:text-indigo-700"
                         >
-                            <span className={`${isCollapsed ? "hidden" : ""} ms-3`}>Time Selection</span>
+                            <span className={`${isCollapsed ? "hidden" : ""} ms-3`}>User Profile</span>
                         </Link>
                         <Link
-                            href="/wanderer/account/profile"
+                            href="/user/booking/1"
                             className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group hover:text-indigo-700"
                         >
-                            <span className={`${isCollapsed ? "hidden" : ""} ms-3`}>Wanderer Profile</span>
-                        </Link>
-                        <Link
-                            href="/wanderer/home"
-                            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group hover:text-indigo-700"
-                        >
-                            <span className={`${isCollapsed ? "hidden" : ""} ms-3`}>Wanderer QR Code</span>
-                        </Link>
-                        <Link
-                            href="/"
-                            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group hover:text-indigo-700"
-                        >
-                            <span className={`${isCollapsed ? "hidden" : ""} ms-3`}>Search Home Page</span>
+                            <span className={`${isCollapsed ? "hidden" : ""} ms-3`}>User Booking with QR Code</span>
                         </Link>
                     </li>
                     {/* Add more links here as needed */}
