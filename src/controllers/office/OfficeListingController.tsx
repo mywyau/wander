92ef851fe526bfab297404faca8bcdbb93b59e2b
mywyau <47248104+mywyau@users.noreply.github.com>
2024-12-30 02:50:04 +0000

    wander-51 Add find all endpoint and add temp placeholder code for working concept

--- src/controllers/office/OfficeListingController.tsx
+++ src/controllers/office/OfficeListingController.tsx
@@ -4,6 +4,7 @@ import { OfficeListing } from '@/types/office/OfficeListing';
 
 class OfficeListingController {
   async submitForm(data: InitiateOfficeListingRequest): Promise<OfficeListing> {
+    
     const pistachioUrl = AppConfig.basePistachioUrl(false);
     const apiUrl = `http://${pistachioUrl}/pistachio/business/office/listing/initiate`;
 
@@ -29,6 +30,35 @@ class OfficeListingController {
       }
 
       const responseData: OfficeListing = await response.json();
+
+      console.log("Successfully submitted:", responseData);
+      return responseData;
+    } catch (error) {
+      console.error("Submission error:", error);
+      throw new Error("Failed to submit the form. Please try again.");
+    }
+  }
+
+
+  async getAllOffices(): Promise<OfficeListing[]> {
+    const pistachioUrl = AppConfig.basePistachioUrl(false);
+    const apiUrl = `http://${pistachioUrl}/pistachio/business/office/listing/find/all`;
+
+    console.log("[OfficeListingController] submitForm called");
+    console.log(apiUrl);
+
+    try {
+      const createRequest = {
+        method: "GET",
+      };
+
+      const response = await fetch(apiUrl, createRequest);
+
+      if (!response.ok) {
+        throw new Error("Failed to submit form");
+      }
+
+      const responseData: OfficeListing[] = await response.json();
 
       console.log("Successfully submitted:", responseData);
       return responseData;
